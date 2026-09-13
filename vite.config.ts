7b64cd07df7583955513ccaa80b718cd6b200c3a
Joshua Luke Parris <130725599+joshualparris@users.noreply.github.com>
2026-09-13 16:18:06 +1000

    Configure Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES === "true" ? "/NebulaDice-Browser/" : "/",
   server: {
     host: "127.0.0.1",
     port: 5173
